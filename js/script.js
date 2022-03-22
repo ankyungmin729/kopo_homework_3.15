560eb18baa43d2c74976ded49e1792330ebc0669
ankyungmin729 <ankyungmin729@gmail.com>
2022-03-22 15:54:04 +0900

    배경이미지 버튼 기능 수정

--- js/script.js
+++ js/script.js
@@ -4,22 +4,26 @@ const btnBg = document.querySelector('.btn--toggle');
 const btnBgReset = document.querySelector('.btn--reset');
 const btnBgGray = document.querySelector('.btn--gray');
 const btnBgImg = document.querySelector('.btn--img');
+const BgContainer = document.querySelector('.bg_img');
 
 btnBg.addEventListener('click', function(){
     document.body.classList.remove('bgcolor--gray','bg_img');
+    BgContainer.classList.remove('bg_img_show');
     document.body.classList.toggle('bgcolor--blue');
 });
 
 btnBgReset.addEventListener('click', function(){
     document.body.classList.remove('bgcolor--gray','bgcolor--blue','bg_img');
+    BgContainer.classList.remove('bg_img_show');
 });
 
 btnBgGray.addEventListener('click',function(){
     document.body.classList.remove('bgcolor--blue','bg_img');
+    BgContainer.classList.remove('bg_img_show');
     document.body.classList.add('bgcolor--gray');
 });
 
 btnBgImg.addEventListener('click',function(){
     document.body.classList.remove('bgcolor--blue','bgcolor--gray');
-    document.body.classList.add('bg_img','bg_img_show');
+    BgContainer.classList.add('bg_img_show');
 })
